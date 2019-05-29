e78ec3ab5da9fed86f9d9ce64cfdb82f91e8b0dc
Maxym Basenko <maxym.basenko@icloud.com>
2019-05-29 18:07:48 +0200

    4.0

--- lambda/custom/anmeldung.js
+++ lambda/custom/anmeldung.js
@@ -5,7 +5,7 @@
 module.exports = {
   'ANMELDUNG_DE_DE': {
     'anmelden': 'Hallo, um dich für das Spiel anzumelden, brauche ich deinen namen. Wie heißt du ?',
-    'name': 'Eine Quarzsäule erhält man, indem in Minecraft ein Quarzblock auf einem Quarzblock platziert wird.',
-    'alter': 'Alle verschiedenen gefärbten Glasscheiben lassen sich craften, indem Glas gleicher Farbe in den unteren beiden Reihen horizontal platziert wird.',
+    'name': 'Hier der Name',
+    'alter': 'Hier wird dann das Alter abgefragt.',
   },
 };
